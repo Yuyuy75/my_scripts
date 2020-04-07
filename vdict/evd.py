2a67b34b03da0cd684c9cce9e3f55533ef7af8e1
yuyuy75 <tommy.nguyen9@mavs.uta.edu>
2020-04-06 19:13:21 -0500

    prints out searched word found in html; when using find, dont put begin offset, makes it slower for some reason

--- vdict/evd.py
+++ vdict/evd.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 
 import requests
+import string
+import sys
 
-res = requests.get( 'https://vdict.com/amiable,1,0,0.html' )
-print( res.text[:] )
+url_prefix      = "https://vdict.com/"
+word            = sys.argv[1]
+eng_to_viet     = ",1,0,0.html"
+viet_to_eng     = ",2,0,0.html"
+viet_to_viet    = ",3,0,0.html"
+search_url      = url_prefix + word + eng_to_viet
+res             = requests.get( search_url )
+
+word_len = len( word )
+word_title = res.text.find( "word_title" )
+skip_word_title = len( "word_title\">" )
+# +2 to exclude \n\t
+start_index_word    = word_title + skip_word_title + 2
+print( res.text[ start_index_word:(start_index_word + word_len) ] )
