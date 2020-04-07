ffbdf8b500574e83d79d5614d2bca178883ac8b3
yuyuy75 <tommy.nguyen9@mavs.uta.edu>
2020-04-06 20:28:18 -0500

    successfully prints out word type, but python is reading them incorrectly..

--- vdict/evd.py
+++ vdict/evd.py
@@ -12,6 +12,7 @@ viet_to_viet    = ",3,0,0.html"
 search_url      = url_prefix + word + eng_to_viet
 res             = requests.get( search_url )
 
+# get searched word
 word_title          = res.text.find( "word_title" )
 skip_to_word        = len( "word_title\">\n\t" )
 start_index_word    = word_title + skip_to_word
@@ -20,9 +21,18 @@ end_index_word      = start_index_word + word_len
 
 print( res.text[start_index_word:end_index_word] )
 
+# get pronunciation
 pronounce           = res.text.find( "pronounce" )
 skip_to_word        = len( "pronounce\">/" )
 start_index_word    = pronounce + skip_to_word
 end_index_word      = res.text.find( "/<", pronounce )
+
+print( res.text[start_index_word:end_index_word] )
+
+# get phanloai
+word_type           = res.text.find( "phanloai" )
+skip_to_word        = len( "phanloai\">" )
+start_index_word    = word_type + skip_to_word
+end_index_word      = res.text.find( "<", word_type )
 
 print( res.text[start_index_word:end_index_word] )
