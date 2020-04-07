26f0ceda7d842ba80ce7464cedd2a6d8b2bde0fe
yuyuy75 <tommy.nguyen9@mavs.uta.edu>
2020-04-06 19:31:15 -0500

    lined up variables for neatness

--- vdict/evd.py
+++ vdict/evd.py
@@ -12,9 +12,10 @@ viet_to_viet    = ",3,0,0.html"
 search_url      = url_prefix + word + eng_to_viet
 res             = requests.get( search_url )
 
-word_len = len( word )
-word_title = res.text.find( "word_title" )
-skip_word_title = len( "word_title\">" )
-# +2 to exclude \n\t
-start_index_word    = word_title + skip_word_title + 2
-print( res.text[ start_index_word:(start_index_word + word_len) ] )
+word_len            = len( word )
+word_title          = res.text.find( "word_title" )
+skip_word_title     = len( "word_title\">\n\t" )
+start_index_word    = word_title + skip_word_title
+end_index_word      = start_index_word + word_len
+
+print( res.text[start_index_word:end_index_word] )
