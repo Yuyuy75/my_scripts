61f631f2d70a90fb844f9ee9c02be8f1f3b79ef8
yuyuy75 <tommy.nguyen9@mavs.uta.edu>
2020-04-29 17:12:09 -0500

    fixed wrong save path for .vimrc

--- bash_scripts/saveconf.sh
+++ bash_scripts/saveconf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 vimrc_path=/home/tri/.vimrc
-vimrc_save_path=/home/tri/docs/my_scripts/bash_scripts/dotfiles/
-cp $vimrc_path "$save_path.vimrc$(date +_%Y-%m-%d_%H:%M:%S)" 
+vimrc_save_path=/home/tri/docs/dotfiles/
+cp $vimrc_path "$vimrc_save_path.vimrc$(date +_%Y-%m-%d_%H:%M:%S)" 
