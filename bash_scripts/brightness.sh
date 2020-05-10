6253c2694a617c6ccc7c55e7cd64b4ea723df9dc
yuyuy75 <tommy.nguyen9@mavs.uta.edu>
2020-05-09 22:15:28 -0500

    added value checking to keep values between 0 and 1

--- bash_scripts/brightness.sh
+++ bash_scripts/brightness.sh
@@ -10,13 +10,25 @@ max_brightness=$brightness
 if [ $# -eq 1 ]
     then
     brightness_percent=$( echo "scale=0; ($1 % 100 * $max_brightness)/1" | bc )
-    echo $brightness_percent
+    if [ $1 -gt 1 ]
+        then
+        echo -e "Brightness value too high!\n\
+Enter a number in the range [0.0... - 1.0...]"
+        exit 2
+    fi
+    if [ $1 -lt 0 ]
+        then
+        echo -e "Brightness value too low!\n\
+Enter a number in the range [0.0... - 1.0...]"
+        exit 2
+    fi
+    #echo $brightness_percent
     sudo sh -c "echo $brightness_percent > $brightness_path"
 exit 0
 fi
 
 echo -e "Wrong number of arguments!\n\n\
-Usage: script [1 - $max_brightness]"
+Usage: script [0.00... - 1.00...]"
 exit 1
 # print error if $1 is greater than max_brightness
 # print error if $# is not exactly 1
