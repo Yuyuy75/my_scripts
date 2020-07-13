6ee54a830786d85494b72f65c31a2ac695a9eccd
yuyuy75 <tommy.nguyen9@mavs.uta.edu>
2020-07-12 23:45:14 -0500

    changed if conditionals.. now have to reduce to only 1 arg

--- bash_scripts/lit.sh
+++ bash_scripts/lit.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
+#   this script changes screen brightness
+#+  works for ubuntu 18.04 with an intel video card
+#+  no guarantees for anything else! :p
 
 brightness_path='/sys/class/backlight/intel_backlight/brightness'
-#   for future reference, this is how you get a... 
-#+  value into a variable
 brightness=$(cat "/sys/class/backlight/intel_backlight/max_brightness")
 max_brightness=$brightness
 
-# need spaces between brackets and inside
+
 if [ $# -eq 1 ]
     then
     brightness_percent=$( echo "scale=0; ($1 % 100 * $max_brightness)/1" | bc )
-    if [ $1 -gt 1 ]
+    #if [ $1 -gt 1 ]
+if [ awk -v bright_val = "$1" BEGIN {printf "$bright_val" > 1 ? "1" : "0"} ]
         then
         echo -e "Brightness value too high!\n\
 Enter a number in the range [0.0... - 1.0...]"
         exit 2
     fi
-    if [ $1 -lt 0 ]
+    if [ awk -v bright_val = "$1" BEGIN {printf "$bright_val" < 1 ? "1" : "0"} ]
         then
         echo -e "Brightness value too low!\n\
 Enter a number in the range [0.0... - 1.0...]"
         exit 2
     fi
-    #echo $brightness_percent
+    #echo $brightness_percent; for testing!
     sudo sh -c "echo $brightness_percent > $brightness_path"
 exit 0
 fi
 
 echo -e "Wrong number of arguments!\n\n\
-Usage: script [0.0... - 1.0...]"
+Usage: script [0.0... - 1.0...]\n
+Example: script .5"
 exit 1
-# print error if $1 is greater than max_brightness
-# print error if $# is not exactly 1
+
+# print error if $1 is greater than max_brightness DONE
+# print error if $# is not exactly 1 DONE
 # add -h and --help flag
+# fix error in which integer expression is expected..
