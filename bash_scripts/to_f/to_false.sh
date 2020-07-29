2b6576a943d051070c1f537fa570b712d3594cd0
yuyuy75 <tommy.nguyen9@mavs.uta.edu>
2020-07-28 20:17:36 -0500

    changed ternary operator to real if else statements; rough printf formatting works

--- bash_scripts/to_f/to_false.sh
+++ bash_scripts/to_f/to_false.sh
@@ -15,6 +15,11 @@ first_ln=$(awk 'NF == 6 { print NR; exit; }' "$my_file")
 # echo "$first_ln"
 
 awk -v start="$first_ln" '
-BEGIN {OFS = "\t"; offset1 = 2; offset2 = 3; F = "F"} 
-{print NR == start + offset1 || NR == start + offset2 ? $1 " " $2 " " $3 " " F " " F " " F : $0; if(NR == start + offset1) offset1 += 4; if(NR == start + offset2) offset2 += 4};
+BEGIN {OFS = "\t"; offset1 = 2; offset2 = 3;}
+{if(NR == start + offset1 || NR == start + offset2) 
+    printf("%6d %6d %6d %s %s %s\n", $1, $2, $3, "F", "F", "F");
+    else
+        print $0; 
+if(NR == start + offset1) offset1 += 4; 
+if(NR == start + offset2) offset2 += 4};
 ' "$my_file"
